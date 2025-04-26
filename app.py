bb993d760017d59e7c6c8d6004ac340df7e84302
sanyue7th <sanyue7th@gmail.com>
2025-04-26 01:06:15 -0400

    Change port for render
    Signed-off-by: sanyue7th <sanyue7th@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000) 
+    # Bind to PORT environment variable or default to 10000, on all interfaces
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
